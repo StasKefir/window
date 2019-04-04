7e875c7ade54ee93a1b9b0068a071e75de749a63
Victor <staskefir99@gmail.com>
2019-04-04 14:24:09 +0300

    all push

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,43 @@
-const path = require('path');
+'use strict';
+
+let path = require('path');
+
 
 module.exports = {
-    context: path.resolve(__dirname, 'src'),
-    entry: './index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'public')
-    },
-    mode: 'development',
-    watch: true,
-    devtool: 'source-map',
+  mode: 'development',
+  entry: './js/script.js',
+  output: {
+    filename: 'bundle.js',
+    path: __dirname + '/dist/js'
+  },
+  watch: true,
 
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        'presets' : [
-                            [
-                                "@babel/preset-env",
-                                {
-                                    "targets":{
-                                        "browsers": ['last 2 version', "ie >= 11"]
-                                    }
-                                }
-                            ]
-                        ]
-                    }
-                }
-            }
-        ]
-    }
-}
+  devtool: "source-map",
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader?optional[]=runtime',
+          options: {
+            presets: [
+              ["@babel/env", {
+                targets: {
+                  edge: "17",
+                  firefox: "60",
+                  chrome: "67",
+                  safari: "11.1",
+                  ie: "11"
+                },
+              }]
+            ],
+            plugins: ["es6-promise"]
+          }
+        }
+      }
+    ]
+  },
+
+};
